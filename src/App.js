6b6af8162279bcd9f582fabfed85563b290b38fd
carolina <carolinacapetilloc@gmail.com>
2023-08-07 16:31:51 -0700

    added desktopdatetimepicker and added a calendar that has the option to add time. Added plugins so that time can be in UTC

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import DownTime from './Components/create-downtime.component';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 function App() {
   return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
     <div className="App">
       <header className="App-header">
         <DownTime/>
       </header>
     </div>
+    </LocalizationProvider>
   );
 }
 
--- src/Components/create-downtime.component.js
+++ src/Components/create-downtime.component.js
@@ -1,5 +1,11 @@
-import React, { useState } from 'react';
-import DatePicker, { handleDateChange } from "react-datepicker";
+import React, { useState } from 'react'; 
+import dayjs from 'dayjs';
+import { DesktopDateTimePicker } from '@mui/x-date-pickers';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 
 const DownTime = () => {
@@ -8,67 +14,34 @@ const DownTime = () => {
         setInputDowntime(event.target.value);
     }; 
 
-    // converting time to UTC time with js method
-    const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState(new Date());
+
+
 
 
     return (
     <div className="downtime-form">
         <h1>Downtimes</h1>
         <div className="site">
-        <input className="input-site" type="text" value={inputDowntime}
-           onChange={handleInputChange} placeholder="Enter a site" />
+            <input className="input-site" type="text" value={inputDowntime}
+            onChange={handleInputChange} placeholder="Enter a site" />
             <button className="btn">ADD</button>
         </div>
         <div className="telescope">
-        <input className="input-telescope" type="text" value={inputDowntime}
-           onChange={handleInputChange} placeholder="Enter a telescope" />
+            <input className="input-telescope" type="text" value={inputDowntime}
+            onChange={handleInputChange} placeholder="Enter a telescope" />
             <button className="btn">ADD</button>
         </div>
-        <DatePicker
-        selected={startDate}
-        onChange={handleDateChange}
-        showTimeSelect
-        dateFormat="Pp"
-            // selected={startDate} 
-            // onChange={(date) => setStartDate(date)}
-            // showTimeSelect
-            // dateFormat="Pp"
+        <DesktopDateTimePicker 
+            timezone='UTC'
         />
-        <DatePicker
-        selected={endDate}
-        onChange={handleDateChange}
-        showTimeSelect
-        dateFormat="Pp"
-            // selected={endDate} 
-            // onChange={(date) => setEndDate(date)}
-            // showTimeSelect
-            // dateFormat="Pp"
+        <DesktopDateTimePicker 
+            timezone='UTC'
         />
-        {/* <div className="startdate">
-        <input className="input-startdate" type="date" value={inputDowntime}
-           onChange={handleInputChange} placeholder="Enter the start date" />
-            <button className="btn">ADD</button>
-        </div>
-        <div className="starttime">
-        <input className="input-starttime" type="time" value={dateTime}
-           onChange={handleInputChange} placeholder="Enter the start date" />
-            <button className="btn">ADD</button>
-        </div>
-        <div className="enddate">
-        <input className="input" type="date" value={inputDowntime}
-           onChange={handleInputChange} placeholder="Enter the end date" />
-            <button className="btn">ADD</button>
-        </div>
-        <div className="endtime">
-        <input className="input-endtime" type="time" value={dateTime}
-           onChange={handleInputChange} placeholder="Enter the start date" />
-            <button className="btn">ADD</button>
-        </div> */}
+  
+
         <div className="reason">
-        <input className="input" type="text" value={inputDowntime}
-           maxLength="255" onChange={handleInputChange} placeholder="Enter a reason. 255" />
+            <input className="input" type="text" value={inputDowntime}
+            maxLength="255" onChange={handleInputChange} placeholder="Enter a reason. 255" />
             <button className="btn">ADD</button>
         </div>
     </div>
