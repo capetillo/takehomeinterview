4da4c406e8338e579c4ea1c9cb3a471a3c14f25e
carolina <carolinacapetilloc@gmail.com>
2023-08-07 14:58:14 -0700

    Added more input forms for site, telescope, start date, end date, and reason. Still in skeleton form

--- src/Components/create-downtime.component.js
+++ src/Components/create-downtime.component.js
@@ -2,16 +2,36 @@ import React, { useState } from 'react';
 
 
 const DownTime = () => {
-    const [inputTask, setInputTask] = useState("");
+    const [inputDowntime, setInputDowntime] = useState("");
     const handleInputChange = (event) => {
-        setInputTask(event.target.value);
+        setInputDowntime(event.target.value);
     }; 
     return (
-    <div className="Todo">
+    <div className="downtime-form">
         <h1>Downtimes</h1>
-        <div className="Top">
-        <input className="input" type="text" value={inputTask}
-           onChange={handleInputChange} placeholder="Enter a task" />
+        <div className="site">
+        <input className="input-site" type="text" value={inputDowntime}
+           onChange={handleInputChange} placeholder="Enter a site" />
+            <button className="btn">ADD</button>
+        </div>
+        <div className="telescope">
+        <input className="input-telescope" type="text" value={inputDowntime}
+           onChange={handleInputChange} placeholder="Enter a telescope" />
+            <button className="btn">ADD</button>
+        </div>
+        <div className="startdate">
+        <input className="input-startdate" type="date" value={inputDowntime}
+           onChange={handleInputChange} placeholder="Enter the start date" />
+            <button className="btn">ADD</button>
+        </div>
+        <div className="enddate">
+        <input className="input" type="date" value={inputDowntime}
+           onChange={handleInputChange} placeholder="Enter the end date" />
+            <button className="btn">ADD</button>
+        </div>
+        <div className="reason">
+        <input className="input" type="text" value={inputDowntime}
+           onChange={handleInputChange} placeholder="Enter a reason" />
             <button className="btn">ADD</button>
         </div>
     </div>
