d3c0afd95a24321ad021fb63c85389bfbbd733c9
carolina <carolinacapetilloc@gmail.com>
2023-08-07 15:32:33 -0700

    Added function to convert time to UTC

--- src/Components/create-downtime.component.js
+++ src/Components/create-downtime.component.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 
-
 const DownTime = () => {
     const [inputDowntime, setInputDowntime] = useState("");
     const handleInputChange = (event) => {
         setInputDowntime(event.target.value);
     }; 
+
+    // converting dates to UTC time with js method
+    const event = new Date;
+    const dateTime = event.toUTCString();
+
     return (
     <div className="downtime-form">
         <h1>Downtimes</h1>
@@ -24,14 +28,24 @@ const DownTime = () => {
            onChange={handleInputChange} placeholder="Enter the start date" />
             <button className="btn">ADD</button>
         </div>
+        <div className="starttime">
+        <input className="input-starttime" type="time" value={dateTime}
+           onChange={handleInputChange} placeholder="Enter the start date" />
+            <button className="btn">ADD</button>
+        </div>
         <div className="enddate">
         <input className="input" type="date" value={inputDowntime}
            onChange={handleInputChange} placeholder="Enter the end date" />
             <button className="btn">ADD</button>
         </div>
+        <div className="endtime">
+        <input className="input-endtime" type="time" value={dateTime}
+           onChange={handleInputChange} placeholder="Enter the start date" />
+            <button className="btn">ADD</button>
+        </div>
         <div className="reason">
         <input className="input" type="text" value={inputDowntime}
-           onChange={handleInputChange} placeholder="Enter a reason" />
+           maxLength="255" onChange={handleInputChange} placeholder="Enter a reason. 255" />
             <button className="btn">ADD</button>
         </div>
     </div>
