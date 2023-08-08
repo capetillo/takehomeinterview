0573bb39ef886e72e8a6b3ead6e76741faa69fcd
carolina <carolinacapetilloc@gmail.com>
2023-08-07 21:21:34 -0700

    added functionality to the button. UI is still in the works

--- src/Components/create-downtime.component.js
+++ src/Components/create-downtime.component.js
@@ -1,22 +1,45 @@
 import React, { useState } from 'react'; 
 import dayjs from 'dayjs';
-import { DesktopDateTimePicker } from '@mui/x-date-pickers';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
+import { DesktopDateTimePicker } from '@mui/x-date-pickers';
+import { v4 as uuidv4 } from 'uuid';
+import Form from 'react-bootstrap/Form'
+import Badge from 'react-bootstrap/Badge'
 
+// dayjs plugins to convert time to utc
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
 
 const DownTime = () => {
+    // initializing state of inputDowntime to empty string 
     const [inputDowntime, setInputDowntime] = useState("");
+    // updating the state of inputDowntime any time the value of the input changes
     const handleInputChange = (event) => {
         setInputDowntime(event.target.value);
     }; 
 
+    // adding functionality to the button 
+    const [list, setList] = useState([])
+    // runs when add button is clicked to add new downtime
+    const handleAddDowntime = (downtime) => {
+        const newDownTime = {
+            downtime: downtime,
+            // unique id generator 
+            id: uuidv4(),
+        }
+    }
 
 
+    //setting limit to 255 characters for reason input below
+    const [inputText, setInputText] = useState("");
+    const [characterLimit] = useState(255);
 
+    // event handler for character limit
+    const handleChange = event => {
+    setInputText(event.target.value);
+  };
 
     return (
     <div className="downtime-form">
@@ -37,13 +60,21 @@ const DownTime = () => {
         <DesktopDateTimePicker 
             timezone='UTC'
         />
-  
 
-        <div className="reason">
-            <input className="input" type="text" value={inputDowntime}
-            maxLength="255" onChange={handleInputChange} placeholder="Enter a reason. 255" />
-            <button className="btn">ADD</button>
-        </div>
+  <Form>
+    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Control as="textarea"  rows={6} value={inputText} placeholder="Reason" onChange={handleChange} isInvalid={(inputText.length > characterLimit)} />
+        <Badge className='mt-3' bg={`${inputText.length > characterLimit ? 'danger' : 'primary'}`}>{inputText.length}/{characterLimit}</Badge>
+        </Form.Group>
+    </Form>
+
+
+     
+
+           
+
+
+
     </div>
     );
 };
