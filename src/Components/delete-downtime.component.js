41cc77a2605a748a17ece5f40cb8d39fb0b96170
carolina <carolinacapetilloc@gmail.com>
2023-08-09 12:10:04 -0700

    added delete functionality to delete-downtime component

---
+++ src/Components/delete-downtime.component.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// id: unique id of downtime entruy to be deleted
+// onDelete: cb function to be called when delete button is clicked so that entry can be deleted
+const DeleteDowntime = ({ id, onDelete }) => {
+// handler function that deletes downtime entry by finding id 
+  const handleDelete = () => {
+    onDelete(id);
+  };
+
+  return (
+    <div>
+      <button onClick={handleDelete}>Delete</button>
+    </div>
+  );
+};
+
+export default DeleteDowntime;
